48538de3c152cb670fcd53c4c6eb134f3ece1714
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-27 10:31:58 -0500

    set up dependencies

---
+++ src/components/List.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [{"id":1, "text":"ham"}, {"id":2, "text":"cheese"}, {id:3,"text":"potatoes"}]
+
+var List = React.createClass({
+  
+})
