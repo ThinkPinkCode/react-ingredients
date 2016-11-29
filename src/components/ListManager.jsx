872fc170953d17afa7c095a1ebed544c072de264
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-29 11:55:31 -0500

    added styling to panel

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -22,13 +22,20 @@ var ListManager = React.createClass({
 
   render: function(){
     return(
-      <div>
-        <h3>{this.props.title}</h3>
+      <div className="col-sm-4">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3>{this.props.title}</h3>
+          </div>
+
+          <div className="panel-body">
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.onChange} value={this.state.newItemText} />
           <button>Add</button>
         </form>
         <List items={this.state.items}/>
+      </div>
+      </div>
       </div>
     )
   }
